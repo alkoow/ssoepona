42f765c0a507e923dfd2c97d86a880b30e64e2e9
alkoow <40866845+alkoow@users.noreply.github.com>
2018-07-10 20:15:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,6 +10,7 @@ bot.on('ready', function() {
 bot.on('message', message => {
     if(message.content[0] === PREFIX) {
         if(message.content === "!compris") {
+		message.reply('test');
            let role = message.guild.roles.find('name', 'Famille');
         message.member.addRole(role);
 		bot.channels.get("464027343594323999").send("Bienvenue " + user + " ! Tu es maintenant une Night Wolves ! Ahouuuuu !") }
