2f806c7ffa7277a395f1d3d13199cb7e7d433e53
alkoow <40866845+alkoow@users.noreply.github.com>
2018-07-10 20:12:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,11 +9,12 @@ bot.on('ready', function() {
 
 bot.on('message', message => {
     if(message.content[0] === PREFIX) {
-        if(message.content === "!compris")
-           let role = message.guild.roles.find('name', 'Famille')
-        message.member.addRole(role)
-		bot.channels.get("464027343594323999").send("Bienvenue " + user + " ! Tu es maintenant une Night Wolves ! Ahouuuuu !")
-    }
+        if(message.content === "!compris") {
+           let role = message.guild.roles.find('name', 'Famille');
+        message.member.addRole(role);
+		bot.channels.get("464027343594323999").send("Bienvenue " + user + " ! Tu es maintenant une Night Wolves ! Ahouuuuu !");
+ }   
+	}
 });
 
 /* bot.on('messageReactionAdd', (reaction, user) => {
