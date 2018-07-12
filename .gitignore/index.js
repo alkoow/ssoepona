2819a3a35cef828a7fa5ebaed104f731aee80e06
alkoow <40866845+alkoow@users.noreply.github.com>
2018-07-12 09:45:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -60,6 +60,6 @@ setInterval(function(){
 			 bot.channels.get("466726740904116235").send("Test Heure");
 			if(minutes === 2)
 				 bot.channels.get("466726740904116235").send("Test Minutes");
-}, 1200000);
+}, 10000);
 
 bot.login(process.env.TOKEN)
